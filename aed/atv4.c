f2035382f26c3646ed3cec75fa4645e9467a04c5
Luis (Fim) <66277412+luisfim@users.noreply.github.com>
2026-08-11 19:58:08 +0000

    atv 4 still not ready

--- aed/atv4.c
+++ aed/atv4.c
@@ -1,1 +1,24 @@
+// Crie um programa recursivo e outro iterativo que calcule o 
+//MDC (máximo divisor comum) de dois números.
+
+#include <string.h>
+#include <stdbool.h>
+
+int num1,num2,mdc, test =2;
+bool end = true;
+
+while (end){
+
+}
+
+int encontrarMDC (int num1, int num2) {
+    if (num1 % test == 0 && num2 % test == 0) {
+        mdc = mdc*test;
+}
+    else if (num1 % test != 0 || num2 % test != 0){
+        test=test+1;
+}
+    else if (num1 % test != 0 && num2 % test != 0){
+        end = 1;
+}
 
